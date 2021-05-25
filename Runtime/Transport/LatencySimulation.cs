827e61dfb11d7e37b73f43ec19bd57ce025daa5c
GitHub Workflow Agent <github-workflow-agent@github.com>
2021-05-25 17:18:45 +0000

    Format for submodule

--- Runtime/Transport/LatencySimulation.cs
+++ Runtime/Transport/LatencySimulation.cs
@@ -21,9 +21,15 @@ namespace Mirror
     {
         public Transport wrap;
 
+        [Header("Common")]
+        [Tooltip("Spike latency via perlin(Time * speedMultiplier) * spikeMultiplier")]
+        [Range(0, 1)] public float latencySpikeMultiplier;
+        [Tooltip("Spike latency via perlin(Time * speedMultiplier) * spikeMultiplier")]
+        public float latencySpikeSpeedMultiplier = 1;
+
         [Header("Reliable Messages")]
         [Tooltip("Reliable latency in seconds")]
-        public float reliableLatency = 0;
+        public float reliableLatency;
         // note: packet loss over reliable manifests itself in latency.
         //       don't need (and can't add) a loss option here.
         // note: reliable is ordered by definition. no need to scramble.
@@ -32,9 +38,9 @@ namespace Mirror
         [Tooltip("Packet loss in %")]
         [Range(0, 1)] public float unreliableLoss;
         [Tooltip("Unreliable latency in seconds")]
-        public float unreliableLatency = 0;
-        [Tooltip("Scramble unreliable messages, just like over the real network. Mirror unreliable is unordered.")]
-        public bool unreliableScramble;
+        public float unreliableLatency;
+        [Tooltip("Scramble % of unreliable messages, just like over the real network. Mirror unreliable is unordered.")]
+        [Range(0, 1)] public float unreliableScramble;
 
         // message queues
         // list so we can insert randomly (scramble)
@@ -60,19 +66,44 @@ namespace Mirror
         void OnEnable() { wrap.enabled = true; }
         void OnDisable() { wrap.enabled = false; }
 
+        // noise function can be replaced if needed
+        protected virtual float Noise(float time) => Mathf.PerlinNoise(time, time);
+
+        // helper function to simulate latency
+        float SimulateLatency(int channeldId)
+        {
+            // spike over perlin noise.
+            // no spikes isn't realistic.
+            // sin is too predictable / no realistic.
+            // perlin is still deterministic and random enough.
+            float spike = Noise(Time.time * latencySpikeSpeedMultiplier) * latencySpikeMultiplier;
+
+            // base latency
+            switch (channeldId)
+            {
+                case Channels.Reliable:
+                    return reliableLatency + spike;
+                case Channels.Unreliable:
+                    return unreliableLatency + spike;
+                default:
+                    return 0;
+            }
+        }
+
         // helper function to simulate a send with latency/loss/scramble
-        void SimulateSend(int connectionId, int channelId, ArraySegment<byte> segment, List<QueuedMessage> reliableQueue, List<QueuedMessage> unreliableQueue)
+        void SimulateSend(int connectionId, int channelId, ArraySegment<byte> segment, float latency, List<QueuedMessage> reliableQueue, List<QueuedMessage> unreliableQueue)
         {
             // segment is only valid after returning. copy it.
             // (allocates for now. it's only for testing anyway.)
             byte[] bytes = new byte[segment.Count];
             Buffer.BlockCopy(segment.Array, segment.Offset, bytes, 0, segment.Count);
+
             // enqueue message. send after latency interval.
             QueuedMessage message = new QueuedMessage
             {
                 connectionId = connectionId,
                 bytes = bytes,
-                time = Time.time
+                time = Time.time + latency
             };
 
             switch (channelId)
@@ -87,8 +118,9 @@ namespace Mirror
                     if (!drop)
                     {
                         // simulate scramble (Random.Next is < max, so +1)
+                        bool scramble = random.NextDouble() < unreliableScramble;
                         int last = unreliableQueue.Count;
-                        int index = unreliableScramble ? random.Next(0, last + 1) : last;
+                        int index = scramble ? random.Next(0, last + 1) : last;
 
                         // simulate latency
                         unreliableQueue.Insert(index, message);
@@ -129,8 +161,11 @@ namespace Mirror
             unreliableClientToServer.Clear();
         }
 
-        public override void ClientSend(int channelId, ArraySegment<byte> segment) =>
-            SimulateSend(0, channelId, segment, reliableClientToServer, unreliableClientToServer);
+        public override void ClientSend(int channelId, ArraySegment<byte> segment)
+        {
+            float latency = SimulateLatency(channelId);
+            SimulateSend(0, channelId, segment, latency, reliableClientToServer, unreliableClientToServer);
+        }
 
         public override Uri ServerUri() => wrap.ServerUri();
 
@@ -140,8 +175,11 @@ namespace Mirror
 
         public override bool ServerDisconnect(int connectionId) => wrap.ServerDisconnect(connectionId);
 
-        public override void ServerSend(int connectionId, int channelId, ArraySegment<byte> segment) =>
-            SimulateSend(connectionId, channelId, segment, reliableServerToClient, unreliableServerToClient);
+        public override void ServerSend(int connectionId, int channelId, ArraySegment<byte> segment)
+        {
+            float latency = SimulateLatency(channelId);
+            SimulateSend(connectionId, channelId, segment, latency, reliableServerToClient, unreliableServerToClient);
+        }
 
         public override void ServerStart()
         {
@@ -168,7 +206,7 @@ namespace Mirror
             {
                 // check the first message time
                 QueuedMessage message = reliableClientToServer[0];
-                if (message.time + reliableLatency <= Time.time)
+                if (message.time <= Time.time)
                 {
                     // send and eat
                     wrap.ClientSend(Channels.Reliable, new ArraySegment<byte>(message.bytes));
@@ -183,7 +221,7 @@ namespace Mirror
             {
                 // check the first message time
                 QueuedMessage message = unreliableClientToServer[0];
-                if (message.time + unreliableLatency <= Time.time)
+                if (message.time <= Time.time)
                 {
                     // send and eat
                     wrap.ClientSend(Channels.Unreliable, new ArraySegment<byte>(message.bytes));
@@ -203,7 +241,7 @@ namespace Mirror
             {
                 // check the first message time
                 QueuedMessage message = reliableServerToClient[0];
-                if (message.time + reliableLatency <= Time.time)
+                if (message.time <= Time.time)
                 {
                     // send and eat
                     wrap.ServerSend(message.connectionId, Channels.Reliable, new ArraySegment<byte>(message.bytes));
@@ -218,7 +256,7 @@ namespace Mirror
             {
                 // check the first message time
                 QueuedMessage message = unreliableServerToClient[0];
-                if (message.time + unreliableLatency <= Time.time)
+                if (message.time <= Time.time)
                 {
                     // send and eat
                     wrap.ServerSend(message.connectionId, Channels.Unreliable, new ArraySegment<byte>(message.bytes));
