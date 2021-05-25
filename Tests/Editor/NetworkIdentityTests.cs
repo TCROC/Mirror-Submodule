967d1b72addcbe534b2b7ef583b40c4b54e29452
GitHub Workflow Agent <github-workflow-agent@github.com>
2021-05-25 17:07:37 +0000

    Format for submodule

--- Tests/Editor/NetworkIdentityTests.cs
+++ Tests/Editor/NetworkIdentityTests.cs
@@ -22,6 +22,14 @@ namespace Mirror.Tests
             }
         }
 
+        class StartServerExceptionNetworkBehaviour : NetworkBehaviour
+        {
+            public override void OnStartServer()
+            {
+                throw new Exception("some exception");
+            }
+        }
+
         // A Test behaves as an ordinary method
         [Test]
         public void OnStartServerTest()
@@ -208,6 +216,162 @@ namespace Mirror.Tests
 
             // clean up
             GameObject.DestroyImmediate(gameObject);
+        }
+
+        [Test]
+        public void RemoveObserverInternal()
+        {
+            // create a networkidentity
+            GameObject gameObject = new GameObject();
+            NetworkIdentity identity = gameObject.AddComponent<NetworkIdentity>();
+
+            // call OnStartServer so that observers dict is created
+            identity.OnStartServer();
+
+            // add an observer connection
+            NetworkConnectionToClient connection = new NetworkConnectionToClient(42);
+            identity.observers[connection.connectionId] = connection;
+
+            // RemoveObserverInternal with invalid connection should do nothing
+            identity.RemoveObserverInternal(new NetworkConnectionToClient(43));
+            Assert.That(identity.observers.Count, Is.EqualTo(1));
+
+            // RemoveObserverInternal with existing connection should remove it
+            identity.RemoveObserverInternal(connection);
+            Assert.That(identity.observers.Count, Is.EqualTo(0));
+
+            // clean up
+            GameObject.DestroyImmediate(gameObject);
+        }
+
+        [Test]
+        public void AssignSceneID()
+        {
+            // create a networkidentity
+            GameObject gameObject = new GameObject();
+            NetworkIdentity identity = gameObject.AddComponent<NetworkIdentity>();
+
+            // Awake will have assigned a random sceneId of format 0x00000000FFFFFFFF
+            // -> make sure that one was assigned, and that the left part was
+            //    left empty for scene hash
+            Assert.That(identity.sceneId, !Is.Zero);
+            Assert.That(identity.sceneId & 0xFFFFFFFF00000000, Is.EqualTo(0x0000000000000000));
+
+            // make sure that Awake added it to sceneIds dict
+            Assert.That(NetworkIdentity.GetSceneIdentity(identity.sceneId), !Is.Null);
+
+            // clean up
+            GameObject.DestroyImmediate(gameObject);
+        }
+
+        [Test]
+        public void SetSceneIdSceneHashPartInternal()
+        {
+            // create a networkidentity
+            GameObject gameObject = new GameObject();
+            NetworkIdentity identity = gameObject.AddComponent<NetworkIdentity>();
+
+            // Awake will have assigned a random sceneId of format 0x00000000FFFFFFFF
+            // -> make sure that one was assigned, and that the left part was
+            //    left empty for scene hash
+            Assert.That(identity.sceneId, !Is.Zero);
+            Assert.That(identity.sceneId & 0xFFFFFFFF00000000, Is.EqualTo(0x0000000000000000));
+            ulong rightPart = identity.sceneId;
+
+            // set scene hash
+            identity.SetSceneIdSceneHashPartInternal();
+
+            // make sure that the right part is still the random sceneid
+            Assert.That(identity.sceneId & 0x00000000FFFFFFFF, Is.EqualTo(rightPart));
+
+            // make sure that the left part is a scene hash now
+            Assert.That(identity.sceneId & 0xFFFFFFFF00000000, !Is.Zero);
+            ulong finished = identity.sceneId;
+
+            // calling it again should said the exact same hash again
+            identity.SetSceneIdSceneHashPartInternal();
+            Assert.That(identity.sceneId, Is.EqualTo(finished));
+
+            // clean up
+            GameObject.DestroyImmediate(gameObject);
+        }
+
+        [Test]
+        public void OnValidateSetupIDsSetsEmptyAssetIDForSceneObject()
+        {
+            // create a networkidentity
+            GameObject gameObject = new GameObject();
+            NetworkIdentity identity = gameObject.AddComponent<NetworkIdentity>();
+
+            // OnValidate will have been called. make sure that assetId was set
+            // to 0 empty and not anything else, because this is a scene object
+            Assert.That(identity.assetId, Is.EqualTo(Guid.Empty));
+
+            // clean up
+            GameObject.DestroyImmediate(gameObject);
+        }
+
+        [Test]
+        public void OnStartServerComponentExceptionIsCaught()
+        {
+            // create a networkidentity with our test component
+            GameObject gameObject = new GameObject();
+            NetworkIdentity identity = gameObject.AddComponent<NetworkIdentity>();
+            gameObject.AddComponent<StartServerExceptionNetworkBehaviour>();
+
+            // an exception in OnStartServer should be caught, so that one
+            // component's exception doesn't stop all other components from
+            // being initialized
+            // (an error log is expected though)
+            LogAssert.ignoreFailingMessages = true;
+            identity.OnStartServer(); // should catch the exception internally and not throw it
+            LogAssert.ignoreFailingMessages = false;
+
+            // clean up
+            GameObject.DestroyImmediate(gameObject);
+        }
+
+        // OnStartServer in host mode should set isClient=true
+        [Test]
+        public void OnStartServerInHostModeSetsIsClientTrue()
+        {
+            // setup a transport so that Connect doesn't get NullRefException
+            // -> needs to be on a GameObject because Connect calls .enabled=true,
+            //    which only works if it's on a gameobject
+            GameObject transportGO = new GameObject();
+            Transport.activeTransport = transportGO.AddComponent<MemoryTransport>();
+
+            // call client connect so that internals are set up
+            // (it won't actually successfully connect)
+            // -> also set up connectmessage handler to avoid unhandled msg error
+            NetworkClient.RegisterHandler<ConnectMessage>(msg => {}, false);
+            NetworkClient.Connect("localhost");
+
+            // manually invoke transport.OnConnected so that NetworkClient.active is set to true
+            Transport.activeTransport.OnClientConnected.Invoke();
+            Assert.That(NetworkClient.active, Is.True);
+
+            // create a networkidentity with our test component
+            GameObject gameObject = new GameObject();
+            NetworkIdentity identity = gameObject.AddComponent<NetworkIdentity>();
+
+            // isClient needs to be true in OnStartServer if in host mode.
+            // this is a test for a bug that we fixed, where isClient was false
+            // in OnStartServer if in host mode because in host mode, we only
+            // connect the client after starting the server, hence isClient would
+            // be false in OnStartServer until way later.
+            // -> we have the workaround in OnStartServer, so let's also test to
+            //    make sure that nobody ever breaks it again
+            Assert.That(identity.isClient, Is.False);
+            identity.OnStartServer();
+            Assert.That(identity.isClient, Is.True);
+
+            // clean up
+            NetworkClient.Disconnect();
+            NetworkServer.Shutdown();
+            Transport.activeTransport = null;
+            GameObject.DestroyImmediate(gameObject);
+            GameObject.DestroyImmediate(transportGO);
         }
     }
 }
