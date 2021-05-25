33630521a793502c26d4312332f702480816d8a3
GitHub Workflow Agent <github-workflow-agent@github.com>
2021-05-25 17:13:44 +0000

    Format for submodule

--- Tests/Editor/NetworkServerTest.cs
+++ Tests/Editor/NetworkServerTest.cs
@@ -37,9 +37,11 @@ namespace Mirror.Tests
 
     struct TestMessage2 : IMessageBase
     {
+#pragma warning disable CS0649 // Field is never assigned to
         public int IntValue;
         public string StringValue;
         public double DoubleValue;
+#pragma warning restore CS0649 // Field is never assigned to
 
         // Mirror will fill out these empty methods
         public void Deserialize(NetworkReader reader) { }
@@ -1156,6 +1158,62 @@ namespace Mirror.Tests
                     Debug.LogError("Could not find function name");
                     break;
             }
+        }
+
+
+        [Test]
+        public void NoConnectionsTest_WithNoConnection()
+        {
+            Assert.That(NetworkServer.NoConnections(), Is.True);
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void NoConnectionsTest_WithConnections()
+        {
+            NetworkServer.connections.Add(1, null);
+            NetworkServer.connections.Add(2, null);
+            Assert.That(NetworkServer.NoConnections(), Is.False);
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(2));
+
+            NetworkServer.connections.Clear();
+        }
+
+        [Test]
+        public void NoConnectionsTest_WithHostOnly()
+        {
+            ULocalConnectionToServer connectionToServer = new ULocalConnectionToServer();
+            ULocalConnectionToClient connectionToClient = new ULocalConnectionToClient();
+            connectionToServer.connectionToClient = connectionToClient;
+            connectionToClient.connectionToServer = connectionToServer;
+
+            NetworkServer.SetLocalConnection(connectionToClient);
+            NetworkServer.connections.Add(0, connectionToClient);
+
+            Assert.That(NetworkServer.NoConnections(), Is.True);
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(1));
+
+            NetworkServer.connections.Clear();
+            NetworkServer.RemoveLocalConnection();
+        }
+
+        [Test]
+        public void NoConnectionsTest_WithHostAndConnection()
+        {
+            ULocalConnectionToServer connectionToServer = new ULocalConnectionToServer();
+            ULocalConnectionToClient connectionToClient = new ULocalConnectionToClient();
+            connectionToServer.connectionToClient = connectionToClient;
+            connectionToClient.connectionToServer = connectionToServer;
+
+            NetworkServer.SetLocalConnection(connectionToClient);
+            NetworkServer.connections.Add(0, connectionToClient);
+            NetworkServer.connections.Add(1, null);
+
+            Assert.That(NetworkServer.NoConnections(), Is.False);
+            Assert.That(NetworkServer.connections.Count, Is.EqualTo(2));
+
+            NetworkServer.connections.Clear();
+            NetworkServer.RemoveLocalConnection();
         }
     }
 }
