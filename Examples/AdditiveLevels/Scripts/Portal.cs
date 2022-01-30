a03a3f4bd2570f0326e6841109ecc284953a645c
GitHub Workflow Agent <github-workflow-agent@github.com>
2022-01-30 23:26:13 +0000

    Format for submodule

--- Examples/AdditiveLevels/Scripts/Portal.cs
+++ Examples/AdditiveLevels/Scripts/Portal.cs
@@ -15,13 +15,8 @@ namespace Mirror.Examples.AdditiveLevels
         [Tooltip("Reference to child TMP label")]
         public TMPro.TextMeshPro label;
 
-        WaitForSeconds waitForSeconds;
-
-        public override void OnStartServer()
-        {
-            // This is aproximately the fade time
-            waitForSeconds = new WaitForSeconds(((AdditiveLevelsNetworkManager)NetworkManager.singleton).fadeInOut.speed + 0.3f);
-        }
+        // This is approximately the fade time
+        WaitForSeconds waitForFade = new WaitForSeconds(1f);
 
         /// <summary>
         /// Called on every NetworkBehaviour when it is activated on a client.
@@ -36,10 +31,10 @@ namespace Mirror.Examples.AdditiveLevels
         // up in the Physics collision matrix so only Player collides with Portal.
         void OnTriggerEnter(Collider other)
         {
-            //Debug.Log($"Portal::OnTriggerEnter {gameObject.name} in {gameObject.scene.name}");
-
             // tag check in case you didn't set up the layers and matrix as noted above
             if (!other.CompareTag("Player")) return;
+
+            //Debug.Log($"{System.DateTime.Now:HH:mm:ss:fff} Portal::OnTriggerEnter {gameObject.name} in {gameObject.scene.name}");
 
             // applies to host client on server or remote client
             if (other.TryGetComponent<PlayerController>(out PlayerController playerController))
@@ -60,11 +55,14 @@ namespace Mirror.Examples.AdditiveLevels
                 // Tell client to unload previous subscene. No custom handling for this.
                 conn.Send(new SceneMessage { sceneName = gameObject.scene.path, sceneOperation = SceneOperation.UnloadAdditive, customHandling = true });
 
-                yield return waitForSeconds;
+                yield return waitForFade;
 
                 //Debug.Log($"SendPlayerToNewScene RemovePlayerForConnection {conn} netId:{conn.identity.netId}");
                 NetworkServer.RemovePlayerForConnection(conn, false);
-                yield return null;
+
+                // reposition player on server and client
+                player.transform.position = startPosition;
+                player.transform.LookAt(Vector3.up);
 
                 // Move player to new subscene.
                 SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByPath(destinationScene));
@@ -72,13 +70,8 @@ namespace Mirror.Examples.AdditiveLevels
                 // Tell client to load the new subscene with custom handling (see NetworkManager::OnClientChangeScene).
                 conn.Send(new SceneMessage { sceneName = destinationScene, sceneOperation = SceneOperation.LoadAdditive, customHandling = true });
 
-                // reposition player on server and client
-                player.transform.position = startPosition;
-                player.transform.LookAt(Vector3.up);
-
-                NetworkServer.AddPlayerForConnection(conn, player);
                 //Debug.Log($"SendPlayerToNewScene AddPlayerForConnection {conn} netId:{conn.identity.netId}");
-                yield return null;
+                NetworkServer.AddPlayerForConnection(conn, player);
 
                 // host client would have been disabled by OnTriggerEnter above
                 if (NetworkClient.localPlayer != null && NetworkClient.localPlayer.TryGetComponent<PlayerController>(out PlayerController playerController))
