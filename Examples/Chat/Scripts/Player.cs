5bbf13d8bdc49d3e9077f6e3aafff948c1098c78
GitHub Workflow Agent <github-workflow-agent@github.com>
2022-01-30 23:26:32 +0000

    Format for submodule

--- Examples/Chat/Scripts/Player.cs
+++ Examples/Chat/Scripts/Player.cs
@@ -7,17 +7,24 @@ namespace Mirror.Examples.Chat
     {
         public static readonly HashSet<string> playerNames = new HashSet<string>();
 
-        public override void OnStartServer()
-        {
-            playerName = (string)connectionToClient.authenticationData;
-        }
-
         [SyncVar(hook = nameof(OnPlayerNameChanged))]
         public string playerName;
+
+        // RuntimeInitializeOnLoadMethod -> fast playmode without domain reload
+        [UnityEngine.RuntimeInitializeOnLoadMethod]
+        static void ResetStatics()
+        {
+            playerNames.Clear();
+        }
 
         void OnPlayerNameChanged(string _, string newName)
         {
             ChatUI.instance.localPlayerName = playerName;
+        }
+
+        public override void OnStartServer()
+        {
+            playerName = (string)connectionToClient.authenticationData;
         }
     }
 }
